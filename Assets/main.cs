abfdf0c60b0d12a6a1a0c2df72030bb371c6ceb2
scott1028 <mic1028002@gmail.com>
2014-03-18 21:48:26 +0800

    修正排序演算法

--- Assets/main.cs
+++ Assets/main.cs
@@ -10,23 +10,25 @@ public class main : MonoBehaviour {
         Transform[] all_cc = new Transform[cc.childCount];
         IOrderedEnumerable<Transform> all_dd;
 
-        for (int i = 0; i < cc.childCount; i++)
-        {
-            all_cc[i] = cc.GetChild(i);
-        }
+        for (int i = 0; i < cc.childCount; i++) all_cc[i] = cc.GetChild(i);
 
         int idx = 0;
+
         // sorting
-        all_dd = all_cc.OrderBy(a => a.transform.localPosition.z).ThenBy(b => b.transform.localPosition.x);
-        foreach (Transform i in all_dd) {
-            Debug.Log(i.localPosition.x);
-            i.gameObject.AddComponent<ball_main>();
-            i.gameObject.AddComponent<ball_animate>();
-            i.gameObject.AddComponent<ball_color_build>();
-            i.GetComponent<ball_main>().id = idx;
-            i.GetComponent<ball_main>().position = new Vector2(Mathf.Repeat(idx, 5), (int)(idx / 5f));
-            idx += 1;
-            i.gameObject.name = idx.ToString();
+        all_dd = all_cc.OrderBy(a => Mathf.Round(a.transform.localPosition.z * 100)).ThenBy(a => Mathf.Round(a.transform.localPosition.x * 100));
+
+        foreach (Transform i in all_dd)
+        {
+            if (i.active)
+            {
+                i.gameObject.AddComponent<ball_main>();
+                i.gameObject.AddComponent<ball_animate>();
+                i.gameObject.AddComponent<ball_color_build>();
+                i.GetComponent<ball_main>().id = idx;
+                i.GetComponent<ball_main>().position = new Vector2(Mathf.Repeat(idx, 6), (int)(idx / 6f));
+                i.gameObject.name+= idx.ToString();
+                idx += 1;
+            }
         }
 	}
 
